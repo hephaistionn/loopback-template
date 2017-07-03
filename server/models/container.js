790486bee057e9678cb3815534bc6e18e13ddce5
hephaistionn <alexandre.monge.mail@gmail.com>
2017-07-03 23:50:32 +0200

    add forgotten files

---
+++ server/models/container.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(Container) {
+    /*
+     * Actions (Remote Methods)
+     */
+    /*
+     * Actions (Remote Hooks)
+     */
+    /*
+     * Actions (Listeners)
+     */
+};
